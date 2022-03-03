d1b5b7ec18ac50915d7efb8967173ae83c3bfe5a
Paul <pauloladapo13@gmail.com>
2022-03-03 11:40:49 +0000

    CRUD

--- BBDD/Ejemplo2BBDD.py
+++ BBDD/Ejemplo2BBDD.py
@@ -5,21 +5,38 @@ mConnetion= sqlite3.connect("ProductStore")
 mCursor= mConnetion.cursor()
 
 
-mCursor.execute(''' CREATE TABLE ProductStore (ITEM_CODE VARCHAR(4) PRIMARY KEY,
-ITEM_NAME VARCHAR(50), PRICE INTEGER,
-SECTION VARCHAR(20))
- ''')
+# mCursor.execute(''' CREATE TABLE ProductStore (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+# NOMBRE_ARTICULO VARCHAR(50) UNIQUE, PRICE INTEGER,
+# SECTION VARCHAR(20))
+#  ''')
 
-products=[
+# products=[
 
-    ("AR01", "trousers",15,"outfit"),
-    ("AR02", "hdmi cable",25,"tecno"),
-    ("AR03", "plate",45,"items"),
-    ("AR04", "pelota",20,"toystore"),
+#     ("trousers",15,"outfit"),
+#     ("hdmi cable",25,"tecno"),
+#     ("plate",45,"items"),
+#     ("pelota",20,"toystore"),
+#     ("jeans",35, "outfit")
+# ]
 
-]
 
-mCursor.executemany("INSERT INTO ProductStore VALUES (?,?,?,?)", products)
+# mCursor.executemany("INSERT INTO ProductStore VALUES (NULL,?,?,?)", products)
+
+# mCursor.execute("INSERT INTO PRODUCTSTORE VALUES (`'AR05','tren', 15, 'transport')")
+
+
+
+# mCursor.execute("UPDATE PRODUCTSTORE SET PRICE= 35 WHERE NOMBRE_ARTICULO='pelota'")
+
+mCursor.execute("DELETE FROM PRODUCTSTORE WHERE ID=5")
+
+
+# products= mCursor.fetchall()
+
+# print(products)
+
+
+
 
 mConnetion.commit()
 
