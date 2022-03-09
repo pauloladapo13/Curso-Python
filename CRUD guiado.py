10ccd7b1fcabd49d1ee5e216467c942ced1bad4e
Paul <pauloladapo13@gmail.com>
2022-03-09 20:47:00 +0000

    CRUD2

--- CRUD guiado.py
+++ CRUD guiado.py
@@ -63,10 +63,44 @@ scrollVert.grid(row=5, column=2, sticky="nsew")
 
 textoComentario.config(yscrollcommand=scrollVert.set)
 
+#-----------------------------aqui comienzan los label------------------------------
+
+idLabel=Label(miFrame, text="Id:")
+idLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+nombreLabel=Label(miFrame, text="Nombre:")
+nombreLabel.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+passLabel=Label(miFrame, text="Password:")
+passLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+
+apellidoLabel=Label(miFrame, text="Apellido:")
+apellidoLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
+
+direccionLabel=Label(miFrame, text="Address:")
+direccionLabel.grid(row=4, column=0, sticky="e", padx=10, pady=10)
+
+comentariosLabel=Label(miFrame, text="Comentarios:")
+comentariosLabel.grid(row=5, column=0, sticky="e", padx=10, pady=10)
 
 
 
+#----------------aqui los botones-------------------
 
+miFrame2=Frame(root)
+miFrame2.pack()
+
+botonCrear=Button(miFrame2, text="Create ")
+botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+botonLeer=Button(miFrame2, text="Read ")
+botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10)
+
+botonUpdate=Button(miFrame2, text="Update ")
+botonUpdate.grid(row=1, column=2, sticky="e", padx=10, pady=10)
+
+botonDelete=Button(miFrame2, text="Delete ")
+botonDelete.grid(row=1, column=3, sticky="e", padx=10, pady=10)
 
 
 root.mainloop()
