950574942f9ada7af2feea30010cf7f7010bff1e
Paul <pauloladapo13@gmail.com>
2022-03-14 11:57:59 +0000

    CRUD4

--- CRUD guiado.py
+++ CRUD guiado.py
@@ -46,13 +46,62 @@ def limpiarCampos():
 def crear():
     miConexion=sqlite3.connect("Usuarios")
 
-    miCursor=miConexion.cursor()
+    miCursor=miConexion.cursor() 
 
     miCursor.execute("INSERT INTO DATOSUSUARIOS VALUES(NULL, '" + miNombre.get() + "','" + miPass.get() + "','" + miApellido.get() + "','" + miDireccion.get() + "','" + textoComentario.get("1.0", END) + "')")
     miConexion.commit()
 
     messagebox.showinfo("BBDD", "Registro insertado con exito")
 
+
+def leer():
+
+    miConexion=sqlite3.connect("Usuarios")
+
+    miCursor=miConexion.cursor() 
+
+    miCursor.execute("SELECT * FROM DATOSUSUARIOS WHERE ID=" + miId.get())
+
+    elUsuario=miCursor.fetchall()
+
+    for usuario in elUsuario: 
+        miId.set(usuario[0])
+        miNombre.set(usuario[1])
+        miPass.set(usuario[2])
+        miApellido.set(usuario[3])
+        miDireccion.set(usuario[4])
+        textoComentario.insert(1.0, usuario[5])
+    miConexion.commit()
+
+
+def actualizar():
+    miConexion=sqlite3.connect("Usuarios")
+
+    miCursor=miConexion.cursor() 
+
+    miCursor.execute("UPDATE DATOSUSUARIOS SET NOMBRE_USUARIO='" + miNombre.get() + 
+        "', PASSWORD='" + miPass.get() + 
+        "', APELLIDO='" + miApellido.get() + 
+        "', DIRECCION='" + miDireccion.get() + 
+        "', COMENTARIOS='" + textoComentario.get("1.0", END) + 
+        "' WHERE ID=" + miId.get())
+
+    miConexion.commit()
+
+    messagebox.showinfo("BBDD", "Registro actualizado con exito")
+
+
+def eliminar():
+
+    miConexion=sqlite3.connect("Usuarios")
+
+    miCursor=miConexion.cursor() 
+
+    miCursor.execute("DELETE FROM DATOSUSUARIOS WHERE ID=" + miId.get())  
+
+    miConexion.commit()
+
+    messagebox.showinfo("BBDD", "Registro borrado con exito")
 
 
 
@@ -72,9 +121,9 @@ borrarMenu.add_command(label="Borrar campos", command=limpiarCampos)
 
 crudMenu = Menu(barraMenu, tearoff=0)
 crudMenu.add_command(label="Crear", command=crear)
-crudMenu.add_command(label="Read")
-crudMenu.add_command(label="Update")
-crudMenu.add_command(label="Delete")
+crudMenu.add_command(label="Read", command=leer)
+crudMenu.add_command(label="Update", command=actualizar)
+crudMenu.add_command(label="Delete", command=eliminar)
 
 
 ayudaMenu = Menu(barraMenu, tearoff=0)
@@ -154,13 +203,13 @@ miFrame2.pack()
 botonCrear = Button(miFrame2, text="Create ", command=crear)
 botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 
-botonLeer = Button(miFrame2, text="Read ")
+botonLeer = Button(miFrame2, text="Read ", command=leer)
 botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10)
 
-botonUpdate = Button(miFrame2, text="Update ")
+botonUpdate = Button(miFrame2, text="Update ", command=actualizar)
 botonUpdate.grid(row=1, column=2, sticky="e", padx=10, pady=10)
 
-botonDelete = Button(miFrame2, text="Delete ")
+botonDelete = Button(miFrame2, text="Delete ", command=eliminar)
 botonDelete.grid(row=1, column=3, sticky="e", padx=10, pady=10)
 
 
